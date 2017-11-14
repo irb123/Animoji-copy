049d62ad24574a3ae33ea515ec25cbcbf8270e56
martint028 <13501326761@163.com>
2017-11-14 17:57:06 +0800

    copy from Animoji-master

---
+++ Example/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Lasha Efremidze on 11/9/17.
+//  Copyright © 2017 Lasha Efremidze. All rights reserved.
+//
+
+import UIKit
+import Animoji
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var animoji: Animoji! {
+        didSet {
+            let name = Animoji.PuppetName.all[0]
+            animoji.setPuppet(name: name)
+        }
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.register(Cell.self, forCellWithReuseIdentifier: "Cell")
+            collectionView.showsVerticalScrollIndicator = false
+            collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            layout.minimumInteritemSpacing = 10
+            layout.minimumLineSpacing = 10
+        }
+    }
+    
+    var layout: UICollectionViewFlowLayout {
+        return collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let itemsPerRow = 4
+        let inset = collectionView.contentInset.left + collectionView.contentInset.right
+        let spacing = layout.minimumInteritemSpacing + layout.minimumLineSpacing
+        let availableWidth = self.view.bounds.width - inset - CGFloat(itemsPerRow - 1) * spacing
+        let width = floor(availableWidth / CGFloat(itemsPerRow))
+        layout.itemSize = CGSize(width: width, height: width)
+    }
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Animoji.PuppetName.all.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+        let name = Animoji.PuppetName.all[indexPath.item]
+        cell.imageView.image = Animoji.thumbnail(forPuppetNamed: name.rawValue)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let name = Animoji.PuppetName.all[indexPath.item]
+        animoji.setPuppet(name: name)
+    }
+}
+
+class Cell: UICollectionViewCell {
+    lazy var imageView: UIImageView = { [unowned self] in
+        let imageView = UIImageView(frame: self.contentView.bounds)
+        imageView.contentMode = .scaleAspectFit
+        self.contentView.addSubview(imageView)
+        return imageView
+    }()
+}
